5b313d072559ad731f09453a053a34168face39d
praful <70875453+Praful-B@users.noreply.github.com>
2025-08-30 18:05:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const faqItems = document.querySelectorAll(".faq-item");
 
-// the logic has been copied from ai
+
 faqItems.forEach((item) => {
   const question = item.querySelector(".faq-question");
   question.addEventListener("click", () => {
